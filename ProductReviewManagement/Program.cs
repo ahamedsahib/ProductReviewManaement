bd5180b801cac417e11eed17d9fe9b4c935ec168
ahamedsahib <adsahib39@gmail.com>
2021-08-06 11:15:49 +0530

    [Ahamed]Add data to List

--- ProductReviewManagement/Program.cs
+++ ProductReviewManagement/Program.cs
@@ -6,7 +6,7 @@ namespace ProductReviewManagement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Product Review");
         }
     }
 }
---
+++ ProductReviewTest/UnitTest1.cs
@@ -0,0 +1,40 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductReviewManagement;
+using System;
+using System.Collections.Generic;
+
+namespace ProductReviewTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        ProductReviewManager productReviewManager;
+        List<ProductReview> reviewList;
+        
+        [TestInitialize]
+        public void SetUp()
+        {
+            reviewList = new List<ProductReview>();
+            productReviewManager = new ProductReviewManager();
+        }
+        /// <summary>
+        /// Test method to create and add reviews to the list
+        /// </summary>
+        [TestMethod]
+        public void TestMethodForAddDataToList()
+        {
+            try
+            {
+                int actual, expected = 25;
+                actual = productReviewManager.AddProductReviewToList(reviewList);
+                Assert.AreEqual(actual, expected);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
